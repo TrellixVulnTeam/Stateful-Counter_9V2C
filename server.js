26ead522c5b8b5604a0704892195befad25f40eb
anyalauria <anyalauria@gmail.com>
2022-05-06 20:58:06 -0700

    Testing new endpoint

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+var clicks = 0;
+
 app.use(express.static('public'));
+
+app.get('/increment', (req, res) => {
+    clicks++;
+    res.send("<h1> The current count is " + clicks + " </h1>");
+  });
 
 /*
 app.get('/', (req, res) => {
