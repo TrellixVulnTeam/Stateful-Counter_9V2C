f435f752c53ed699041810b95574e11e1020d45a
anyalauria <anyalauria@gmail.com>
2022-05-09 15:14:55 -0700

    added persistence

--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 const express = require('express')
+//const sqlite = require('sqlite3').verbose()
+
 const app = express();
 const port = 3000;
 
-var clicks = 0;
+var fs = require('fs');
 
+var clicks;
+
+fs.readFile('count.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    clicks = parseInt(data);
+});
+//let db = new sqlite.Database('./count.db')
+
+//db.run('INSERT into clicks(number_of_clicks)values(clicks)')
 app.use(express.static('public'));
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-*/
 
 app.get('/increment', (req, res) => {
     clicks++;
     console.log("request to increment", clicks)
     res.send({'clicks':clicks});
+
+    fs.writeFile("count.txt", clicks.toString(),'utf8', function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+      });
   });
 
 app.get('/decrement', (req, res) => {
     clicks--;
     console.log("request to decrement", clicks)
     res.send({'clicks':clicks});
+
+    fs.writeFile("count.txt", clicks.toString(), 'utf8', function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+      });
   });
 
 app.listen(port, () => {
